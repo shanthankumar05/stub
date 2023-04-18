007a10b2fa6a62d9b847c7855390c4a1e2c21cff
S.Shanthan Kumar <88592536+shanthankumar05@users.noreply.github.com>
2023-04-18 12:53:15 +0530

    Create great.py

---
+++ great.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a value: ");
+b=int(input("Enter b value : ");
+c=int(input("enter b value: ");
+if ((a>b)&&(a>c)):
+      printf(a)
+elif ((b>c)&&(a>c)):
+      print(b)
+else:
+      print(c)
